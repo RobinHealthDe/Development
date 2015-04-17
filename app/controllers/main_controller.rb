2a6b0f1b0430e12012b07cebd1702b8e2f573520
Ingelheim <lukas.ingelheim@gmail.com>
2015-04-17 11:26:52 +0200

    @ingelheim INITIAL_SETUP Changed password for accessing site on Heroku

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,5 +1,5 @@
 class MainController < ApplicationController
-	http_basic_authenticate_with name: "robinhealth", password: "password"
+	http_basic_authenticate_with name: "rbenning@arzthelden.de", password: "Test#1Test#1"
 
 	def home
 	end

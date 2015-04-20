0f419b7245088b8977a7eb15329df416269e566a
cameel <cameel2@gmail.com>
2015-04-20 19:05:14 +0200

    main_controller.rb Reindent using two spaces instead of tabs

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,6 @@
 class MainController < ApplicationController
-	http_basic_authenticate_with name: "rbenning@arzthelden.de", password: "Test#1Test#1"
+  http_basic_authenticate_with name: "rbenning@arzthelden.de", password: "Test#1Test#1"
 
-	def home
-	end
+  def home
+  end
 end

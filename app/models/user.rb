24e67a10bafd9c290af656a95b444c29421c261a
cameel <cameel2@gmail.com>
2015-04-21 18:09:23 +0200

    Style clean-up for auto-generated User and its migration

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,18 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  schema_associations
+
+  devise(
+    :database_authenticatable,
+    :registerable,
+    :recoverable,
+    :rememberable,
+    :trackable,
+    :validatable,
+
+    # Other available modules:
+    #:confirmable,
+    #:lockable,
+    #:timeoutable,
+    #:omniauthable
+  )
 end

efb377176eb9a664be2f42c613b7321c35727e8f
cameel <cameel2@gmail.com>
2015-04-21 18:21:30 +0200

    [M] Fix and clean up User broken by active_admin
    - Remove unnecessary ActiveAdmin migration for User. It's the same as our own User except that it's missing the timestamp.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,15 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, 
-         :recoverable, :rememberable, :trackable, :validatable
   schema_associations
 
   devise(
     :database_authenticatable,
-    
     :recoverable,
     :rememberable,
     :trackable,
     :validatable,
+
+    # Disabled by ActiveAdmin
+    #:registerable
 
     # Other available modules:
     #:confirmable,
